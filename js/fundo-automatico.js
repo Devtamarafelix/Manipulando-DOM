b0fe62cd06e5306f19c4151f2a7fcdbe5ca82f36
Dev Tâmara <tamarafelix63@gmail.com>
2026-08-11 20:20:13 -0300

    atualiza link js

--- js/fundo-automatico.js
+++ js/fundo-automatico.js
@@ -1,34 +1,31 @@
-// Função para atualizar o fundo de forma automática
 function atualizarFundoAutomatico() {
-  // Pegando a data atual
   const date = new Date()
-
-  // pegando a hora atual em tempo real
+  
+  // Pegando a hora atual
   const hora = date.getHours()
 
-  // Reseto o fundo antes pra imagem da manhã não sobrescrever as outras
-  document.body.style.backgroundImage = "none"
-
-  // Se for de manhã (6h até 11h59), coloca a imagem
+  // Manhã (06h às 11h59)
   if (hora >= 6 && hora < 12) {
-    document.body.style.backgroundImage = "url('./assets/imagem-manha.jpg')"
+    document.body.style.backgroundImage = "url('assets/imagem-manha.jpg')"
     document.body.style.backgroundSize = "cover"
+    document.body.style.backgroundPosition = "center"
 
-  // Se for de tarde (12h até 17h59), coloca o gradiente de tarde
+  // Tarde (12h às 17h59)
   } else if (hora >= 12 && hora < 18) {
-    document.body.style.background = "linear-gradient(145deg, #FF8A65, #FFB74D, #64B5F6)"
+    document.body.style.backgroundImage = "linear-gradient(145deg, #FF8A65, #FFB74D, #64B5F6)"
 
-  // Se for de noite (18h até 23h59), coloca o gradiente noturno
+  // Noite (18h às 23h59)
   } else if (hora >= 18 && hora < 24) {
-    document.body.style.background = "linear-gradient(155deg, #000428, #211132, #004E92)"
+    document.body.style.backgroundImage = "linear-gradient(155deg, #000428, #211132, #004E92)"
 
-  // Se for madrugada (00h até 05h59), coloca esse
+  // Madrugada (00h às 05h59)
   } else {
-    document.body.style.background = "linear-gradient(155deg, #0b7c51, #3f0d74, #183f60)"
+    document.body.style.backgroundImage = "linear-gradient(155deg, #0b7c51, #3f0d74, #183f60)"
   }
 }
 
+// Chama a função na hora que a página carrega
 atualizarFundoAutomatico()
 
-// Rodo a função a cada 1 minuto (60000ms) pra checar se a hora mudou 
+// Atualiza a cada 1 minuto
 setInterval(atualizarFundoAutomatico, 60000)
